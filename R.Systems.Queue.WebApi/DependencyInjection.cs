a0eafa0a594a87619892e8ac8ec6b603d312107b
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2023-11-13 18:59:47 +0100

    feat: add listeners

--- R.Systems.Queue.WebApi/DependencyInjection.cs
+++ R.Systems.Queue.WebApi/DependencyInjection.cs
@@ -1,4 +1,9 @@
-﻿namespace R.Systems.Queue.WebApi;
+﻿using Azure.Messaging.ServiceBus;
+using R.Systems.Queue.Infrastructure.ServiceBus.Common;
+using R.Systems.Queue.Infrastructure.ServiceBus.Options;
+using R.Systems.Queue.WebApi.Listeners;
+
+namespace R.Systems.Queue.WebApi;
 
 public static class DependencyInjection
 {
@@ -7,7 +12,7 @@ public static class DependencyInjection
         services.AddControllers();
         services.AddEndpointsApiExplorer();
         services.ConfigureSwagger();
-        //services.ConfigureServiceBusListeners();
+        services.ConfigureServiceBusListeners();
     }
 
     private static void ConfigureSwagger(this IServiceCollection services)
@@ -15,14 +20,21 @@ public static class DependencyInjection
         services.AddSwaggerGen();
     }
 
-    //private static void ConfigureServiceBusListeners(this IServiceCollection services)
-    //{
-    //    services.ConfigureServiceBusQueueListener<SitesBatchListener, GroupedSitesQueueOptions>(
-    //        new ServiceBusProcessorOptions
-    //        {
-    //            MaxAutoLockRenewalDuration = TimeSpan.FromMinutes(10),
-    //            MaxConcurrentCalls = 1
-    //        }
-    //    );
-    //}
+    private static void ConfigureServiceBusListeners(this IServiceCollection services)
+    {
+        services.ConfigureServiceBusQueueListener<CompanyQueueListener, CompanyQueueOptions>(
+            new ServiceBusProcessorOptions
+            {
+                MaxAutoLockRenewalDuration = TimeSpan.FromMinutes(10),
+                MaxConcurrentCalls = 1
+            }
+        );
+        services.ConfigureServiceBusTopicListener<CompanyTopicListener, CompanyTopicOptions>(
+            new ServiceBusProcessorOptions
+            {
+                MaxAutoLockRenewalDuration = TimeSpan.FromMinutes(10),
+                MaxConcurrentCalls = 1
+            }
+        );
+    }
 }
